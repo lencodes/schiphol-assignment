cff2c9b067c1bfcc77125cf522a8fcf2a161935e
Lencodes <contact@lencodes.com>
2024-12-04 16:49:44 +0100

    INIT App adding a page component and basic styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,14 @@
+import './theme/global.css';
+import './App.css';
+
+import { SearchFlightsPage } from './pages/SearchFlightsPage/SearchFlightsPage';
+
 function App() {
-  return <>Initiate Frontend Assignement</>;
+  return (
+    <div className="app">
+      <SearchFlightsPage />
+    </div>
+  );
 }
 
 export default App;
