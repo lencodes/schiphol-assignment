8a1b4e6c193fd4e7f9b09493246bf94876a60133
Lencodes <contact@lencodes.com>
2024-12-04 17:53:00 +0100

    ADD Error Boundary component and implement

---
+++ src/components/ErrorBoundary/ErrorBoundary.ts
@@ -0,0 +1,38 @@
+import React, { ErrorInfo, ReactNode } from 'react';
+
+interface ErrorBoundaryProps {
+  fallback: ReactNode;
+  children: ReactNode;
+}
+
+interface ErrorBoundaryState {
+  hasError: boolean;
+}
+
+class ErrorBoundary extends React.Component<
+  ErrorBoundaryProps,
+  ErrorBoundaryState
+> {
+  constructor(props: ErrorBoundaryProps) {
+    super(props);
+    this.state = { hasError: false };
+  }
+
+  static getDerivedStateFromError(): ErrorBoundaryState {
+    return { hasError: true };
+  }
+
+  componentDidCatch(error: Error, info: ErrorInfo): void {
+    console.error(error, info.componentStack); // this should be logged somehwere, but out-of-scope for this assignemnt
+  }
+
+  render(): ReactNode {
+    if (this.state.hasError) {
+      return this.props.fallback;
+    }
+
+    return this.props.children;
+  }
+}
+
+export default ErrorBoundary;
